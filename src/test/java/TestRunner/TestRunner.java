a383eb7a6fca8b0226fc81dccce5f3fd1a79563d
yxvnxy <rickybima16@gmail.com>
2022-07-03 22:49:42 +0700

    clientportaltesting_login.feature

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features=".../src/test/resources/features/stockbit_register.feature",
+@CucumberOptions(features=".../src/test/resources/features/clientportaltesting_login.feature",
         glue= {"StepDefinition"},
         plugin ={"pretty","json:target/cucumber.json"})
 public class TestRunner {
